50182adc7d1a25b66745e2e8b8dc4bead6a59c61
adebadayo <adebayanen@gmail.com>
2021-10-22 02:25:47 +0900

    vuejs template追加

---
+++ app/javascript/Exam.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="position-relative">
+    <div class="container">
+      <div class="main justify-content-center d-flex align-items-center">
+        <div class="row">
+          <div class="description col-lg-8 offset-lg-2 col-12">
+          <span class="description">{{description}}</span>
+          </div>
+          <div class="ansewer col-8 offset-2">
+            <form>
+              <div class="input-group input-group-lg mb-3">
+              <span class="input-group-text">{{ruby_class_name}}#</span>
+                <input autofocus="" class="form-control" tupe="text" />
+                <button v-on:click="countUp" class="btn btn-outline-secondary" tupe="button">解答</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div></template>
+
+<script>
+export default {
+  props: {
+    description: { type: String, default: '' },
+    ruby_class_name: { type: String, default: '' },
+  },
+  data: function () {
+    return {
+      message: "Vue!",
+    }
+  }
+}
+</script>
---
+++ app/javascript/packs/exam.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import Exam from '../Exam.vue'
+import App from "../app";
+
+document.addEventListener('DOMContentLoaded', () => {
+  const app = new Vue({
+    el: '#exam',
+    render: h => h(Exam, { props: document.getElementById("exam").dataset})
+  }).$mount()
+  document.body.appendChild(app.$el)
+
+  console.log(app)
+})
