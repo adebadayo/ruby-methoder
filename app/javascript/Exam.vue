eec2cb4d85ec8a7125bca0bd5b06489509cc7529
adebadayo <adebayanen@gmail.com>
2021-10-22 03:06:23 +0900

    解答を表示

--- app/javascript/Exam.vue
+++ app/javascript/Exam.vue
@@ -6,30 +6,54 @@
           <div class="description col-lg-8 offset-lg-2 col-12">
           <span class="description">{{description}}</span>
           </div>
-          <div class="ansewer col-8 offset-2">
-            <form>
+          <div class="answer col-lg-6 offset-lg-3 col-8 offset-2">
+            <form onsubmit="return false;">
               <div class="input-group input-group-lg mb-3">
               <span class="input-group-text">{{ruby_class_name}}#</span>
-                <input autofocus="" class="form-control" tupe="text" />
-                <button v-on:click="countUp" class="btn btn-outline-secondary" tupe="button">解答</button>
+              <input autofocus="" class="form-control" tupe="text" />
+              <button v-on:click="answered = true" class="btn btn-outline-secondary" tupe="button">解答</button>
               </div>
             </form>
+          </div>
+          <div class="correct-answer col-lg-6 offset-lg-3 col-8 offset-2" v-if="answered">
+            <h3 style="display:inline;" v-if="answered" class="description">{{ruby_method_name}}</h3>
+            <button v-on:click="" class="btn btn-outline-secondary" tupe="button">次の問題</button>
           </div>
         </div>
       </div>
     </div>
-  </div></template>
+  </div>
+</template>
 
 <script>
 export default {
   props: {
     description: { type: String, default: '' },
     ruby_class_name: { type: String, default: '' },
+    ruby_method_name: { type: String, default: '' },
   },
   data: function () {
     return {
       message: "Vue!",
+      answered: false
     }
-  }
+  },
 }
 </script>
+
+<style>
+  .main{
+    margin: 200px 0 50px 0;
+  }
+  .description{
+    padding: 50px 0 50px 0;
+  }
+  /*.correct-answer{*/
+  /*  position: absolute;*/
+  /*  right: 0;*/
+  /*  !*padding: 30px 0 30px 0;*!*/
+  /*}*/
+  /*.correct-answer h3{*/
+  /*  padding: 0px 20px 0px 20px;*/
+  /*}*/
+</style>
